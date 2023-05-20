3eb4d0778a676e76c333503ff7fb45d6255ccea6
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-05-20 08:57:04 -0300

    feature: conectando as aplicações ao PostgreSQL

---
+++ stateful/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user(id, username, password) VALUES (1, 'user.test', '$2a$10$BmZwXCdMK7io43ZC3BQsG.wVpEOPYMuNi4ecOQYOuPPqApMMcXTM6');
+INSERT INTO public.user(id, username, password) VALUES (2, 'user.new.test', '$2a$10$BmZwXCdMK7io43ZC3BQsG.wVpEOPYMuNi4ecOQYOuPPqApMMcXTM6');
